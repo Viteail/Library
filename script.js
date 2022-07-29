d72b33023c5fa650a53780acd02909767aaba9c2
Viteail <vitea.sirbu1@gmail.com>
2022-07-29 08:54:05 +0300

    add read btn

--- script.js
+++ script.js
@@ -2,33 +2,32 @@ const submitBtn = document.querySelector("#submit");
 const inputTitle = document.querySelector("#title");
 const inputAuthor = document.querySelector("#author");
 const inputPages = document.querySelector("#page");
-const inputRead = document.querySelector("#read");
 const cardsWrapper = document.querySelector(".books");
 const addBookBtn = document.querySelector(".addbookbtn");
 const modal = document.querySelector(".modal");
 const cancelBtn = document.querySelector("#cancel");
 
 submitBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-  return addBookToLibrary();
+  if (
+    inputTitle.value !== "" &&
+    inputAuthor.value !== "" &&
+    inputPages.value !== ""
+  ) {
+    modal.style.display = "none";
+    return addBookToLibrary();
+  }
 });
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
 }
 
 function addBookToLibrary() {
-  let newBook = new Book(
-    inputTitle.value,
-    inputAuthor.value,
-    inputPages.value,
-    inputRead.value
-  );
+  let newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value);
   myLibrary.push(newBook);
   console.log(myLibrary);
   displayBook();
@@ -45,20 +44,36 @@ function displayBook() {
   paraAuthor.classList.add("author");
   const paraPages = document.createElement("p");
   paraPages.classList.add("page");
-  const paraRead = document.createElement("p");
-  paraRead.classList.add("read");
+
+  const bookBtns = document.createElement("div");
+  bookBtns.classList.add("bookbtns");
+
+  const readBtn = document.createElement("button");
+  readBtn.textContent = "Not read";
+  readBtn.classList.add("readbtn");
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Remove";
+  removeBtn.classList.add("removebtn");
+
+  readBtn.addEventListener('click', () => {
+    readBtn.classList.toggle('read');
+    if (readBtn.classList.contains('read')) {
+      readBtn.textContent = "Read";
+    } else readBtn.textContent = "Not read";
+  });
 
   cardsWrapper.appendChild(card);
   card.appendChild(paraTitle);
   card.appendChild(paraAuthor);
   card.appendChild(paraPages);
-  card.appendChild(paraRead);
+  card.appendChild(bookBtns);
+  bookBtns.appendChild(readBtn);
+  bookBtns.appendChild(removeBtn);
 
   myLibrary.forEach((book) => {
     paraTitle.textContent = '"' + book.title + '"';
     paraAuthor.textContent = book.author;
     paraPages.textContent = book.pages + " " + "pages";
-    paraRead.textContent = book.read;
   });
 }
 
@@ -66,21 +81,20 @@ addBookBtn.addEventListener("click", () => {
   modal.style.display = "block";
 });
 
-window.addEventListener('click', (e) => {
+window.addEventListener("click", (e) => {
   if (e.target == modal) {
     modal.style.display = "none";
     deleteInputValues();
   }
 });
 
-cancelBtn.addEventListener('click', () => {
+cancelBtn.addEventListener("click", () => {
   modal.style.display = "none";
   deleteInputValues();
-})
+});
 
 function deleteInputValues() {
   inputTitle.value = "";
   inputAuthor.value = "";
   inputPages.value = "";
-  inputRead.value = "";
-};
+}
