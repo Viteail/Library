30c0ba65a5ce715cc4c80eb557e0b9eff55cb3a5
Viteail <vitea.sirbu1@gmail.com>
2022-08-24 08:31:44 +0300

    change to use class instead of plain constructor

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-const submitBtn = document.querySelector("#submit");
-const inputTitle = document.querySelector("#title");
-const inputAuthor = document.querySelector("#author");
-const inputPages = document.querySelector("#page");
-const cardsWrapper = document.querySelector(".books");
-const addBookBtn = document.querySelector(".addbookbtn");
-const modal = document.querySelector(".modal");
-const cancelBtn = document.querySelector("#cancel");
+const submitBtn = document.querySelector('#submit');
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const inputPages = document.querySelector('#page');
+const cardsWrapper = document.querySelector('.books');
+const addBookBtn = document.querySelector('.addbookbtn');
+const modal = document.querySelector('.modal');
+const cancelBtn = document.querySelector('#cancel');
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener('click', () => {
   if (
-    inputTitle.value !== "" &&
-    inputAuthor.value !== "" &&
-    inputPages.value !== ""
+    inputTitle.value !== '' &&
+    inputAuthor.value !== '' &&
+    inputPages.value !== ''
   ) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
     return addBookToLibrary();
   }
 });
 
 let myLibrary = [];
 
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
 }
 
 function addBookToLibrary() {
@@ -36,33 +38,33 @@ function addBookToLibrary() {
 }
 
 function displayBook() {
-  const card = document.createElement("div");
-  card.classList.add("book");
-  const paraTitle = document.createElement("p");
-  paraTitle.classList.add("title");
-  const paraAuthor = document.createElement("p");
-  paraAuthor.classList.add("author");
-  const paraPages = document.createElement("p");
-  paraPages.classList.add("page");
+  const card = document.createElement('div');
+  card.classList.add('book');
+  const paraTitle = document.createElement('p');
+  paraTitle.classList.add('title');
+  const paraAuthor = document.createElement('p');
+  paraAuthor.classList.add('author');
+  const paraPages = document.createElement('p');
+  paraPages.classList.add('page');
 
-  const bookBtns = document.createElement("div");
-  bookBtns.classList.add("bookbtns");
+  const bookBtns = document.createElement('div');
+  bookBtns.classList.add('bookbtns');
 
-  const readBtn = document.createElement("button");
-  readBtn.textContent = "Not read";
-  readBtn.classList.add("readbtn");
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "Remove";
-  removeBtn.classList.add("removebtn");
+  const readBtn = document.createElement('button');
+  readBtn.textContent = 'Not read';
+  readBtn.classList.add('readbtn');
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
+  removeBtn.classList.add('removebtn');
 
-  readBtn.addEventListener("click", () => {
-    readBtn.classList.toggle("read");
-    if (readBtn.classList.contains("read")) {
-      readBtn.textContent = "Read";
-    } else readBtn.textContent = "Not read";
+  readBtn.addEventListener('click', () => {
+    readBtn.classList.toggle('read');
+    if (readBtn.classList.contains('read')) {
+      readBtn.textContent = 'Read';
+    } else readBtn.textContent = 'Not read';
   });
 
-  removeBtn.addEventListener("click", () => {
+  removeBtn.addEventListener('click', () => {
     card.remove();
   });
 
@@ -77,28 +79,28 @@ function displayBook() {
   myLibrary.forEach((book) => {
     paraTitle.textContent = '"' + book.title + '"';
     paraAuthor.textContent = book.author;
-    paraPages.textContent = book.pages + " " + "pages";
+    paraPages.textContent = book.pages + ' ' + 'pages';
   });
 }
 
-addBookBtn.addEventListener("click", () => {
-  modal.style.display = "block";
+addBookBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
 });
 
-window.addEventListener("click", (e) => {
+window.addEventListener('click', (e) => {
   if (e.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
     deleteInputValues();
   }
 });
 
-cancelBtn.addEventListener("click", () => {
-  modal.style.display = "none";
+cancelBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
   deleteInputValues();
 });
 
 function deleteInputValues() {
-  inputTitle.value = "";
-  inputAuthor.value = "";
-  inputPages.value = "";
+  inputTitle.value = '';
+  inputAuthor.value = '';
+  inputPages.value = '';
 }
